a1e0b0a7db6f6180ec86c25bd8df0f34863ca5d2
Ahmet Karamanoğlu <ahmet@Ahmet-MacBook-Air.local>
2022-02-10 17:19:44 +0300

    added delete animation

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
     @IBOutlet weak var tableview: UITableView!
     
     
@@ -17,14 +19,30 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         // Do any additional setup after loading the view.
         tableview.delegate = self
         tableview.dataSource = self
+        
+        landmarkNames.append("Rize")
+        landmarkNames.append("İstanbul")
+        landmarkNames.append("Çorum")
+        
+        landmarkImages.append(UIImage(named: "rize")!)
+        landmarkImages.append(UIImage(named: "istanbul")!)
+        landmarkImages.append(UIImage(named: "corum")!)
+
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableview.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return landmarkNames.count
     }
 
 
